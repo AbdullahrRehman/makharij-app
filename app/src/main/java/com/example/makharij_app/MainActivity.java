30d875ecb5de15bc6928385411fc5c5bda3aca1a
Abdullah Rehman <rehmanabdullah945@gmail.com>
2021-11-09 02:53:15 +0500

    Makharij Button enabeld

--- app/src/main/java/com/example/makharij_app/MainActivity.java
+++ app/src/main/java/com/example/makharij_app/MainActivity.java
@@ -12,11 +12,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Button repoButton;
+    private Button makharijButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         repoButton = (Button)findViewById(R.id.repoButton);
+        makharijButton = (Button)findViewById(R.id.makharijButton);
+
 
         repoButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,10 +27,21 @@ public class MainActivity extends AppCompatActivity {
                 openWebPage("https://github.com/AbdullahrRehman/makharij-app");
             }
         });
+        makharijButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInsideActivity4();
+            }
+        });
     }
     public void openWebPage(String url) {
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+    public void openInsideActivity4()
+    {
+        Intent intent = new Intent(this,makharij_page.class);
         startActivity(intent);
     }
 
