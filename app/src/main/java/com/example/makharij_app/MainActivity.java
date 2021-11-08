2af93b647f8923cfc7aa33b875801722e9e69401
Abdullah Rehman <rehmanabdullah945@gmail.com>
2021-11-09 02:33:38 +0500

    Repo Button Enabeld

--- app/src/main/java/com/example/makharij_app/MainActivity.java
+++ app/src/main/java/com/example/makharij_app/MainActivity.java
@@ -2,16 +2,33 @@ package com.example.makharij_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button repoButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        repoButton = (Button)findViewById(R.id.repoButton);
+
+        repoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage("https://github.com/AbdullahrRehman/makharij-app");
+            }
+        });
     }
-
-
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
 
 }
