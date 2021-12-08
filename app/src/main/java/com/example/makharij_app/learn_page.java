b007cc80ee604bf57526d1ec4f713f89a2768172
gmdin <gmdin716@gmail.com>
2021-12-08 14:22:32 +0500

    ActionBar and Menu Completed

--- app/src/main/java/com/example/makharij_app/learn_page.java
+++ app/src/main/java/com/example/makharij_app/learn_page.java
@@ -1,15 +1,22 @@
 package com.example.makharij_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class learn_page extends AppCompatActivity {
 
-
+    Toolbar toolbar;
     private Button halqiyahButton;
     private Button lahatiyahButton;
     private Button tarfiyahButton;
@@ -22,6 +29,11 @@ public class learn_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_page);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
 
         halqiyahButton = (Button)findViewById(R.id.button);
         lahatiyahButton = (Button)findViewById(R.id.button2);
@@ -95,5 +107,25 @@ public class learn_page extends AppCompatActivity {
     {
         Intent intent = new Intent(this,shajariyah_page.class);
         startActivity(intent);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if (id==R.id.action_tasks){
+            Toast.makeText(this, "Task Clicked", Toast.LENGTH_SHORT).show();
+        } else if(id==R.id.action_settings){
+            Toast.makeText(this, "Setting clicked", Toast.LENGTH_SHORT).show();
+        }else if(id==R.id.about){
+            Toast.makeText(this, "Learn More Clicked", Toast.LENGTH_SHORT).show();
+        } else if (id==R.id.exit){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
